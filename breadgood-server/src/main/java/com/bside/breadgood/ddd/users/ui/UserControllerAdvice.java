528a02a4ad66916ce3eeadd8e3d88ccc79f02d26
김도영 <95doyoung@gmail.com>
2021-12-20 15:06:14 +0900

    fix (build) : config 에 있는 properties 이전 경로로 build

--- breadgood-server/src/main/java/com/bside/breadgood/ddd/users/ui/UserControllerAdvice.java
+++ breadgood-server/src/main/java/com/bside/breadgood/ddd/users/ui/UserControllerAdvice.java
@@ -2,27 +2,15 @@ package com.bside.breadgood.ddd.users.ui;
 
 import com.bside.breadgood.common.exception.ExceptionAdvice;
 import com.bside.breadgood.common.exception.ExceptionResponse;
-import com.bside.breadgood.ddd.termstype.application.excetion.IllegalTermsTypeException;
-import com.bside.breadgood.ddd.termstype.application.excetion.NotChoiceRequireTermsTypeException;
 import com.bside.breadgood.ddd.users.application.exception.DuplicateUserNickNameException;
 import com.bside.breadgood.ddd.users.application.exception.OnlySocialLinkException;
 import com.bside.breadgood.ddd.users.application.exception.UserNotFoundException;
-import com.bside.breadgood.jwt.application.exception.AccessTokenExpiredException;
-import com.bside.breadgood.jwt.application.exception.NotFoundRefreshToken;
-import com.bside.breadgood.jwt.application.exception.RefreshTokenExpiredException;
-import com.bside.breadgood.jwt.application.exception.TokenRefreshException;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureException;
-import io.jsonwebtoken.UnsupportedJwtException;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-
-import java.util.Date;
 
 @RestControllerAdvice
 public class UserControllerAdvice extends ExceptionAdvice {
