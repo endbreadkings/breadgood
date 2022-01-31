357426fefd995b9574166f0f3584208e794d25b3
김도영 <doyoung0205@naver.com>
2022-01-31 18:40:10 +0900

    init

---
+++ gof-design-study/src/test/java/me/doyoung/gofdesignstudy/GofDesignStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.doyoung.gofdesignstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofDesignStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
