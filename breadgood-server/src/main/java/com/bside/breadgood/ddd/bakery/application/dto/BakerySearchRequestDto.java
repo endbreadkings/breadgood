60048c60af4f83f01dda30b7c0c5dd5f3a72dd6b
younger-k <younger9403@gmail.com>
2022-02-13 23:51:36 +0900

    fix: 커밋 잘못한거 롤백..

--- breadgood-server/src/main/java/com/bside/breadgood/ddd/bakery/application/dto/BakerySearchRequestDto.java
+++ breadgood-server/src/main/java/com/bside/breadgood/ddd/bakery/application/dto/BakerySearchRequestDto.java
@@ -3,24 +3,22 @@ package com.bside.breadgood.ddd.bakery.application.dto;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.Set;
 
 @Getter
 @ToString
-@NoArgsConstructor
 public class BakerySearchRequestDto {
 
     @ApiModelProperty(position = 1, dataType = "String", example = "서울특별시", notes = "필터가 없을 땐 빈값 넣어주세요.")
-    private String city;
+    private final String city;
 
     @ApiModelProperty(position = 2, dataType = "String", example = "강서구", notes = "필터가 없을 땐 빈값 넣어주세요.")
-    private String district;
+    private final String district;
 
     @ApiModelProperty(position = 3, dataType = "Set", example = "[1, 2]", notes = "필터를 적용하고 싶지 않을 떈 빈 배열을 넣어주세요.")
-    private Set<Long> bakeryCategories;
+    private final Set<Long> bakeryCategories;
 
     @Builder
     public BakerySearchRequestDto(String city, String district, Set<Long> bakeryCategories) {
