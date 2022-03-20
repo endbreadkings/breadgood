038de50080922b005a831b18acdef4daef700864
김도영 <doyoung0205@naver.com>
2022-03-21 03:05:27 +0900

    fix : JpaAuditing 추가 및 BaseEntity  생성시간 수정시간 올바르게 나오도록 수정

--- breadgood-server/src/main/java/com/bside/breadgood/BreadgoodServerApplication.java
+++ breadgood-server/src/main/java/com/bside/breadgood/BreadgoodServerApplication.java
@@ -4,7 +4,9 @@ import com.bside.breadgood.authentication.AuthRedirectUrisProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @EnableConfigurationProperties(AuthRedirectUrisProperties.class)
 @SpringBootApplication
 public class BreadgoodServerApplication {
--- breadgood-server/src/main/java/com/bside/breadgood/common/domain/BaseEntity.java
+++ breadgood-server/src/main/java/com/bside/breadgood/common/domain/BaseEntity.java
@@ -1,31 +1,34 @@
 package com.bside.breadgood.common.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
-@Setter
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
+
     @Column(updatable = false)
+    @CreatedDate
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime created_at;
+
+    @LastModifiedDate
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime updated_at;
 
-    @PrePersist
-    public void prePersist() {
-        LocalDateTime now = LocalDateTime.now();
-        created_at = now;
-        updated_at = now;
-    }
-
-    @PreUpdate
-    public void preUpdate() {
-        updated_at = LocalDateTime.now();
-    }
 }
