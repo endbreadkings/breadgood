e544053bcc4f088dcb3746e71fad5921d144ad93
younger-k <younger9403@gmail.com>
2022-05-29 20:59:47 +0900

    refactor: 해동님 리뷰 반영 - orderBy jpql 제거 & 테스트 메서드 변경

--- breadgood-server/src/main/java/com/bside/breadgood/ddd/breadstyles/infra/BreadStyleRepository.java
+++ breadgood-server/src/main/java/com/bside/breadgood/ddd/breadstyles/infra/BreadStyleRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 
 public interface BreadStyleRepository extends CrudRepository<BreadStyle, Long> {
 
-    @Query("select b from BreadStyle b order by b.sortNumber asc")
-    List<BreadStyle> findAllOrderBySortNumberAsc();
+    List<BreadStyle> findAllByOrderBySortNumberAsc();
 }
