95871128fedb4fdd4f6c98a47e3cbdca0f60e226
younger-k <younger9403@gmail.com>
2022-06-05 23:59:33 +0900

    fix: 이모지 ImgUrl 타입 변경 (String -> ImageUrl)

--- breadgood-server/src/test/java/com/bside/breadgood/fixtures/emoji/EmojiFixture.java
+++ breadgood-server/src/test/java/com/bside/breadgood/fixtures/emoji/EmojiFixture.java
@@ -8,11 +8,24 @@ import com.bside.breadgood.ddd.emoji.domain.Emoji;
  * description :
  */
 public class EmojiFixture {
-    public static final Emoji 이모지1 =
-            new Emoji(
-                    1L,
-                    "emojiName",
-                    "img.url",
-                    1
-            );
+  public static final Emoji 이모지_100 =
+      new Emoji(
+          "별로에요",
+          "https://d74hbwjus7qtu.cloudfront.net/admin/e1.png",
+          100
+      );
+
+  public static final Emoji 이모지_200 =
+      new Emoji(
+          "음...?",
+          "https://d74hbwjus7qtu.cloudfront.net/admin/e2.png",
+          100
+      );
+
+  public static final Emoji 이모지_300 =
+      new Emoji(
+          "괜찮아요",
+          "https://d74hbwjus7qtu.cloudfront.net/admin/e3.png",
+          100
+      );
 }
