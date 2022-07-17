0388c404b1723cb44e1eada550bacbce4c77db0c
mienne <miennes3@gmail.com>
2022-07-17 16:25:37 +0900

    UVNX-216 빵집 검색 > 띄워쓰기 수정

--- breadgood_app/lib/modules/register_bakery/screens/search_bakery.dart
+++ breadgood_app/lib/modules/register_bakery/screens/search_bakery.dart
@@ -11,7 +11,6 @@ import 'dart:core';
 import 'package:breadgood_app/modules/register_bakery/model/bakery_data.dart';
 import 'package:breadgood_app/modules/register_bakery/controller/bakery_controller.dart';
 import 'package:breadgood_app/utils/services/rest_api_service.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 Future<NaverMapData> fetchSearchData(String searchKeyword) async {
   var endpointUrl = 'https://openapi.naver.com/v1/search/local.json';
@@ -293,7 +292,6 @@ class _BakeryCardState extends State<BakeryCard> {
                           SizedBox(
                             child: WordBreakText(
                                 widget.selectedBakery.roadAddress,
-                                spacing: 6,
                                 style: TextStyle(
                                     fontSize: 12.0, color: Color(0xFFA4A4A4))),
                           )
