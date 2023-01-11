c5ccc445af92dc9e0d3c52be9c8f52b92ceea08a
mienne <miennes3@gmail.com>
2023-01-11 20:21:26 +0900

    chore: 추가된 라이브러리 설정파일 변경

--- breadgood_app/lib/services/api/api_path.dart
+++ breadgood_app/lib/services/api/api_path.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/foundation.dart';
 
-const String url = kReleaseMode
-    ? 'https://api.breadgood.com'
-    : 'https://api.breadgood.com';
+// TODO: - Dev, Release URL 추가
+const String url =
+    kReleaseMode ? 'https://api.breadgood.com' : 'https://api.breadgood.com';
 
 const String version = "/api/v1";
 
-const String restApiUrl = "${url}${version}";
+const String restApiUrl = '${url}${version}';
 
 const String kakaoLoginUrl =
-    'https://api.breadgood.com/oauth2/authorization/kakao?redirect_uri=breadgoodapp://breadgood.com/login/oauth2/success';
+    '$url/oauth2/authorization/kakao?redirect_uri=breadgoodapp://breadgood.com/login/oauth2/success';
 
 const String appleLoginUrl =
-    'https://api.breadgood.com/oauth2/authorization/apple?redirect_uri=breadgoodapp://breadgood.com/login/oauth2/success';
+    '$url/oauth2/authorization/apple?redirect_uri=breadgoodapp://breadgood.com/login/oauth2/success';
